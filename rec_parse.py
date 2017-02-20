5ffdd1dae1f726b846e622056b186147852e7f53
scolobey <ryan-goodwin@cookpad.com>
2017-02-20 11:51:33 -0800

    switched to tags

--- rec_parse.py
+++ rec_parse.py
@@ -3,6 +3,12 @@ import numpy as np
 
 phrase_key = {}
 phrase_key_index = 0
+recipes = []
+
+def clear_data():
+    phrase_key = {}
+    phrase_key_index = 0
+    recipes = []
 
 def parse_recipes():
     global phrase_key
@@ -34,23 +40,57 @@ def parse_recipes():
 
     return trainX
 
+def initialize_csv(csv_name):
+    #need to populate phrase_key and phrase_key_index before this.
+    global phrase_key
+    global phrase_key_index
+    global recipes
+
+    with open(csv_name, 'r') as f:
+        data = list(csv.reader(f, delimiter=","))
+        data = data[1:]
+        for x in data:
+            phrase_set = []
+            for y in x[3:]:
+                phrase_set = phrase_set + (y[1:-1].split(","))
+            parsed_set = []
+            for z in phrase_set:
+                if z in phrase_key:
+                    parsed_set.append(phrase_key[z])
+                else:
+                    parsed_set.append(phrase_key_index)
+                    phrase_key[z] = phrase_key_index
+                    phrase_key_index = phrase_key_index + 1
+            recipes.append(parsed_set)
+        return len(data) 
+
+def generate_training_data():
+    trainX = np.zeros((len(recipes), phrase_key_index), dtype=np.int)
+
+    for i, rec in enumerate(recipes):
+        for j, phrase in enumerate(rec):
+            trainX[i][phrase] += 1
+
+    return trainX
+
+
 def parse_newest_recipes():
     global phrase_key
     global phrase_key_index
 
     with open("rec_recent_1000.csv", 'r') as f:
         data = list(csv.reader(f, delimiter=","))
-        data = data[1:] 
+        data = data[1:]
         recipes = []
 
         for x in data:
             phrase_set = []
             clipped = x[3:]
             parsed_set = []
-            
+
             for y in clipped:
                 phrase_set = phrase_set + (y[1:-1].split(","))
-            
+
             for z in phrase_set:
                 if z in phrase_key:
                     parsed_set.append(phrase_key[z])
@@ -67,7 +107,7 @@ def parse_newest_recipes():
 def convert_to_id(id_set):
     recipe_set = []
     with open('rec_recent_1000.csv') as csvfile:
-        
+
         reader = list(csv.reader(csvfile, delimiter=","))
         reader = reader[1:]
         for i, row in enumerate(reader):
@@ -81,16 +121,16 @@ def split(list):
     testLength = len(list)//10+1
     trainLength = len(list)-testLength
 
-    trainX = np.zeros((trainLength, dataLength), dtype=np.int)
-    testX = np.empty((testLength, dataLength), dtype=np.int)
+    train = np.zeros((trainLength, dataLength), dtype=np.int)
+    test = np.empty((testLength, dataLength), dtype=np.int)
 
     artificial_train_index = 0
     artificial_test_index = 0
     for i, recipe in enumerate(list):
         if i%10 == 0:
-            testX[artificial_test_index] = recipe
+            test[artificial_test_index] = recipe
             artificial_test_index += 1
         else:
-            trainX[artificial_train_index] = recipe
+            train[artificial_train_index] = recipe
             artificial_train_index += 1
-    return trainX, testX
+    return train, test
